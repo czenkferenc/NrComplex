3a757eb461989f2b8fe0db53c3ddfb912646605f
Feri Czenk <feri.czenk@hotmail.com>
2020-04-09 18:51:07 +0300

    Add project files.

---
+++ NrComplex/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NrComplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            complex c1 = new complex();
+            complex c2 = new complex(2,3);
+            complex c3 = new complex(4,5);
+            complex c4 = new complex(0,-2);
+            complex c5 = new complex(2);
+
+            //testa afisare
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
+            Console.WriteLine(c3.ToString());
+            Console.WriteLine(c4.ToString());
+            Console.WriteLine(c5.ToString());
+            Console.WriteLine();
+
+            //test calcule 
+            c1 = c2 + c3;
+            Console.WriteLine(c1.ToString());
+            c1 = c2 - c3;
+            Console.WriteLine(c1.ToString());
+            c1 = c2 * c3;
+            Console.WriteLine(c1.ToString());
+            c1 = c3 ^ 3;
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine(c1.trigForm()); // forma trigonometrica
+            Console.WriteLine(c3.trigForm(3)); // forma trigonometrica x^n
+            Console.WriteLine(c3.trigForm(3, false)); // forma trigonometrica x^n calculata cu sau fara schimbarea valorii
+            Console.WriteLine(c3.ToString());
+            Console.WriteLine(c3.trigForm(3, true)); // forma trigonometrica x^n calculata cu sau fara schimbarea valorii
+            Console.WriteLine(c3.ToString());
+            Console.WriteLine();
+           
+        }
+    }
+}
---
+++ NrComplex/complex.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NrComplex
+{
+    class complex
+    {
+        private double a, b;
+
+        public complex() : base()
+        {
+            a = 0; b = 0;
+        }
+
+        public complex(double a)
+        {
+            this.a = a;
+        }
+
+        public complex(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double Real
+        {
+            get { return a; }
+        }
+
+        public double Imaginary
+        {
+            get { return b; }
+        }
+
+        public static complex operator + (complex x, complex y)
+        {
+            complex s = new complex();
+            s.a = x.a + y.a;
+            s.b = x.b + y.b;
+            return s;
+        }
+
+        public static complex operator - (complex x, complex y)
+        {
+            complex s = new complex();
+            s.a = x.a - y.a;
+            s.b = x.b - y.b;
+            return s;
+        }
+
+        public static complex operator * (complex x, complex y)
+        {
+            complex p = new complex();
+            p.a = x.a * y.a - x.b * y.b;
+            p.b = x.a * y.b + x.b * y.a;
+            return p;
+        }
+
+        public static complex operator ^ (complex x, int pow)
+        {
+            complex p = new complex();
+            p = x;
+            for (int i = 1; i < pow; i++)
+            {
+                p *= x;
+            }
+            return p;
+        }
+
+        public override string ToString()
+        {
+            string nr;
+            if (a != 0 || b != 0)
+            {
+                if (a != 0) nr = a.ToString();
+                else nr = "";
+                if (b > 0 && a != 0) nr = nr + "+" + b.ToString() + "i";
+                else if (b > 0 && a == 0) nr = b.ToString() + "i";
+                else if (b < 0) nr = nr + b.ToString() + "i";
+                else nr = nr + "";
+            }
+            else nr = "0";
+            return nr;
+        }
+
+        public string trigForm() // forma trigonometrica
+        {
+            string nr;
+            double r = Math.Sqrt(a * a + b * b);
+            double teta = Math.Atan2(b, a);
+            nr = r.ToString() + "(cos(" + teta.ToString() + ") + i*sin(" + teta.ToString() + "))";
+            return nr;
+        }
+
+        public string trigForm(int n) // forma trigonometrica x^n
+        {
+            string nr;
+            double r = Math.Sqrt(a * a + b * b);
+            double teta = Math.Atan2(b, a);
+            nr =n.ToString() + "*" + r.ToString() + "("+ n.ToString() + "*" + "cos(" + teta.ToString() + ")"+ n.ToString() + "*" + "+ i*sin(" + teta.ToString() + "))";
+            return nr;
+        }
+
+        public string trigForm(int n, bool calculate) // forma trigonometrica x^n calculata cu sau fara schimbarea valorii 
+        {
+            if (calculate == false)
+            {
+                //nu schimba valorea 
+                double ta = a;
+                double tb = b;
+                complex c = new complex(a, b);
+                c = c ^ n;
+                a = c.Real;
+                b = c.Imaginary;
+                string nr;
+                double r = Math.Sqrt(a * a + b * b);
+                double teta = Math.Atan2(b, a);
+                nr = r.ToString() + "(cos(" + teta.ToString() + ") + i*sin(" + teta.ToString() + "))";
+                a = ta;
+                b = tb;
+                return nr;
+            }
+            else
+            {
+                //schimba valoarea
+                double ta = a;
+                double tb = b;
+                complex c = new complex(a, b);
+                c = c ^ n;
+                a = c.Real;
+                b = c.Imaginary;
+                string nr;
+                double r = Math.Sqrt(a * a + b * b);
+                double teta = Math.Atan2(b, a);
+                nr = r.ToString() + "(cos(" + teta.ToString() + ") + i*sin(" + teta.ToString() + "))";
+                return nr;
+            }
+        }
+    }
+}
